9efb4dda1314edb1f95b712770bce8ffa3b6b5ca
ErikNoren05 <91731222+ErikNoren05@users.noreply.github.com>
2022-09-13 13:10:51 +0200

    small cahnges to text

--- Skola/src/schoolSystem.cpp
+++ Skola/src/schoolSystem.cpp
@@ -151,7 +151,7 @@ void schoolSystem::removeStudent()
 
 void schoolSystem::menu()
 {
-	std::cout << "what do you want to do? \n" << "1. new student \n" << "2. new class \n" << "3. add student to class \n" << "4. look for student \n" << "5. information about student \n" << "6. remove student from class \n" << "7. remove student from system \n" << "8. information about class \n" << "9. avsluta \n";
+	std::cout << "what do you want to do? \n" << "1. new student \n" << "2. new class \n" << "3. add student to class \n" << "4. search for student \n" << "5. information about student \n" << "6. remove student from class \n" << "7. remove student from system \n" << "8. information about class \n" << "9. avsluta \n";
 }
 
 
